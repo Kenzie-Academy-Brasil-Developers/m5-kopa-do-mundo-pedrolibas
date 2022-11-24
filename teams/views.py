539bfb033ea06b8315e3b3004663c320908238e4
pedrolibas <pedrogv03@hotmail.com>
2022-11-24 01:28:06 -0300

    feat: create routes create and list teams

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
+from .models import Team
+from django.forms.models import model_to_dict
 
-# Create your views here.
+class TeamView(APIView):
+    def post(self, request):
+        team = Team.objects.create(**request.data)
+
+        return Response(model_to_dict(team), status.HTTP_201_CREATED)
+
+
+    def get(self, request):
+        return Response(Team.objects.all().values())
